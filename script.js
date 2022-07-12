8b7f23061d8288a35ea7edc05afe4c16105d7e12
Kristina Soskova <soskovachristina@yandex.ru>
2022-07-12 23:56:07 +0300

    Проектная работа 4

---
+++ script.js
@@ -0,0 +1,30 @@
+let pop = document.querySelector('.popup');
+let edit = document.querySelector('.profile-intro__edit-button');
+let saveBth = document.querySelector('.popup__save-button');
+let closeBth = document.querySelector('.popup__icon');
+let nameU = document.querySelector('.profile-intro__text-title');
+let job = document.querySelector('.profile-intro__text-subtitle');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.user-name');
+let jobInput = document.querySelector('.job');
+
+function open() {
+    pop.classList.toggle('popup_opened');
+}
+
+nameInput.value = nameU.textContent;
+jobInput.value = job.textContent;
+
+edit.addEventListener('click', open);
+closeBth.addEventListener('click', open);
+saveBth.addEventListener('click', open);
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    nameU.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+}
+
+
+formElement.addEventListener('submit', formSubmitHandler); 
