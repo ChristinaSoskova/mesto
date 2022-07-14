271d6c6574e349467f659284ce4a5c883e70f0ec
Kristina Soskova <soskovachristina@yandex.ru>
2022-07-14 14:48:05 +0300

    Проектная работа 4 - правки 2.0

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 let pop = document.querySelector('.popup');
 let edit = document.querySelector('.intro__edit-button');
 let saveBth = document.querySelector('.popup__save-button');
-let closeBth = document.querySelector('.popup__icon');
+let closeBth = document.querySelector('.popup__close-button');
 let nameU = document.querySelector('.intro__title');
 let job = document.querySelector('.intro__subtitle');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__point-name');
-let jobInput = document.querySelector('.popup__point-job');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__job');
 
 function open() {
-    pop.classList.toggle('popup_opened');
+    pop.classList.add('popup_opened');
     nameInput.value = nameU.textContent;
     jobInput.value = job.textContent;
 }
 
-edit.addEventListener('click', open);
-
 
 function close () {
-    pop.classList.toggle('popup_opened');
+    pop.classList.remove('popup_opened');
 }
-
-
-closeBth.addEventListener('click', close);
-saveBth.addEventListener('click', close);
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -34,3 +27,6 @@ function formSubmitHandler(evt) {
 
 
 formElement.addEventListener('submit', formSubmitHandler); 
+edit.addEventListener('click', open);
+closeBth.addEventListener('click', close);
+saveBth.addEventListener('click', close);
